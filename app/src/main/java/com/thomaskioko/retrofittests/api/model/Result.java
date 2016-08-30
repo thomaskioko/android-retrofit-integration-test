f5cd9e37bdf86ab54b82ae3ca0fb89d378a7670c
Thomas Kioko <kiokotomas@gmail.com>
2016-08-30 16:05:33 +0300

    Create object classes.

---
+++ app/src/main/java/com/thomaskioko/retrofittests/api/model/Result.java
@@ -0,0 +1,287 @@
+package com.thomaskioko.retrofittests.api.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Thomas Kioko
+ */
+public class Result implements Parcelable {
+
+    @SerializedName(value = "poster_path")
+    private String posterPath;
+    private Boolean adult;
+    private String overview;
+    @SerializedName(value = "release_date")
+    private String releaseDate;
+    @SerializedName(value = "genre_ids")
+    private List<Integer> genreIds = new ArrayList<>();
+    private Integer id;
+    @SerializedName(value = "original_title")
+    private String originalTitle;
+    @SerializedName(value = "original_language")
+    private String originalLanguage;
+    private String title;
+    @SerializedName(value = "backdrop_path")
+    private String backdropPath;
+    private Double popularity;
+    @SerializedName(value = "vote_count")
+    private Integer voteCount;
+    private Boolean video;
+    @SerializedName(value = "vote_average")
+    private Double voteAverage;
+
+    /**
+     * Default Constructor
+     */
+    public Result() {
+
+    }
+
+    /**
+     * @return The posterPath
+     */
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    /**
+     * @param posterPath The poster_path
+     */
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    /**
+     * @return The adult
+     */
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    /**
+     * @param adult The adult
+     */
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    /**
+     * @return The overview
+     */
+    public String getOverview() {
+        return overview;
+    }
+
+    /**
+     * @param overview The overview
+     */
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    /**
+     * @return The releaseDate
+     */
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    /**
+     * @param releaseDate The release_date
+     */
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    /**
+     * @return The genreIds
+     */
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    /**
+     * @param genreIds The genre_ids
+     */
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    /**
+     * @return The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return The originalTitle
+     */
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    /**
+     * @param originalTitle The original_title
+     */
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    /**
+     * @return The originalLanguage
+     */
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    /**
+     * @param originalLanguage The original_language
+     */
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    /**
+     * @return The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return The backdropPath
+     */
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    /**
+     * @param backdropPath The backdrop_path
+     */
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    /**
+     * @return The popularity
+     */
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    /**
+     * @param popularity The popularity
+     */
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    /**
+     * @return The voteCount
+     */
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    /**
+     * @param voteCount The vote_count
+     */
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    /**
+     * @return The video
+     */
+    public Boolean getVideo() {
+        return video;
+    }
+
+    /**
+     * @param video The video
+     */
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    /**
+     * @return The voteAverage
+     */
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    /**
+     * @param voteAverage The vote_average
+     */
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.posterPath);
+        dest.writeString(this.overview);
+        dest.writeString(this.releaseDate);
+        dest.writeInt(this.id);
+        dest.writeString(this.originalTitle);
+        dest.writeString(this.originalLanguage);
+        dest.writeString(this.title);
+        dest.writeString(this.backdropPath);
+        dest.writeDouble(this.popularity);
+        dest.writeInt(this.voteCount);
+        dest.writeDouble(this.voteAverage);
+    }
+
+    protected Result(Parcel in) {
+        this.posterPath = in.readString();
+        this.overview = in.readString();
+        this.releaseDate = in.readString();
+        this.id = in.readInt();
+        this.originalTitle = in.readString();
+        this.originalLanguage = in.readString();
+        this.title = in.readString();
+        this.backdropPath = in.readString();
+        this.popularity = in.readDouble();
+        this.voteCount = in.readInt();
+        this.voteAverage = in.readDouble();
+    }
+
+    public static final Creator<Result> CREATOR = new Creator<Result>() {
+        @Override
+        public Result createFromParcel(Parcel source) {
+            return new Result(source);
+        }
+
+        @Override
+        public Result[] newArray(int size) {
+            return new Result[size];
+        }
+    };
+}
