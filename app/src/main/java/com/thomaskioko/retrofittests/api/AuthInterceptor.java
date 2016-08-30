3b42436e5617c54b3fae591b1544e7ebbb7798b1
Thomas Kioko <kiokotomas@gmail.com>
2016-08-30 16:04:06 +0300

    Create Authenticator class.
    This class will add the API to our retrofit object

---
+++ app/src/main/java/com/thomaskioko/retrofittests/api/AuthInterceptor.java
@@ -0,0 +1,36 @@
+package com.thomaskioko.retrofittests.api;
+
+
+import com.thomaskioko.retrofittests.BuildConfig;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * This class add information (API Key) to {@link okhttp3.OkHttpClient} which is passed in
+ * {@link TmdbApiClient#getRestAdapter()} which is required when making a request.
+ *
+ * @author Thomas Kioko
+ */
+public class AuthInterceptor implements Interceptor {
+
+    /**
+     * Default constructor.
+     */
+    public AuthInterceptor() {
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        HttpUrl url = request.url().newBuilder()
+                .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+                .build();
+        request = request.newBuilder().url(url).build();
+        return chain.proceed(request);
+    }
+}
